556525185011c47dddd2ee18c3f671a92b8487a9
Paxa <pavel.kanavalau@gmail.com>
2017-05-24 19:51:31 +0300

    add copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-const path = require('path');
 const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
@@ -9,4 +11,28 @@ module.exports = {
     path: path.resolve(__dirname, './dist/scripts'),
     filename: 'bundle.js',
   },
+  devServer: {
+      contentBase: path.resolve(__dirname, './src'),
+  },
+  plugins: [
+      new CopyWebpackPlugin([
+          { from: 'index.html', to: 'dist/index.html' },
+      ], {
+          ignore: [
+              // // Doesn't copy any files with a txt extension
+              // '*.txt',
+              //
+              // // Doesn't copy any file, even if they start with a dot
+              // '**/*',
+              //
+              // // Doesn't copy any file, except if they start with a dot
+              // { glob: '**/*', dot: false }
+          ],
+
+          // By default, we only copy modified files during
+          // a watch or webpack-dev-server build. Setting this
+          // to `true` copies all files.
+          copyUnmodified: true
+      })
+  ]
 };
