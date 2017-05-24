42e8f6da3992617f1584cbc278accd9abd57d78c
VallyPepyako <vally.pepyako@gmail.com>
2017-05-24 19:25:46 +0300

    add postcss/cssnext

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   context: path.resolve(__dirname, './src'),
+
   entry: {
     app: './scripts/app.js',
   },
@@ -9,4 +12,17 @@ module.exports = {
     path: path.resolve(__dirname, './dist/scripts'),
     filename: 'bundle.js',
   },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1!postcss-loader'
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
 };
